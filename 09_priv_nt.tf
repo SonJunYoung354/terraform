72907c3e210240a302a00e5d4364579879f86e6d
erty7640@gmail.com <erty7640@gmail.com>
2021-11-11 16:22:52 +0900

    step1

--- 09_priv_nt.tf
+++ 09_priv_nt.tf
@@ -2,11 +2,11 @@
 ## add router table subnet 
 resource "aws_route_table_association" "Son_nta"{
   subnet_id = aws_subnet.Son_pria.id
-  route_table_id = aws_route_table.Son_rf.id
+  route_table_id = aws_route_table.Son_router_nt.id
 }
 
 resource "aws_route_table_association" "Son_ntc" {
   subnet_id = aws_subnet.Son_pric.id
-  route_table_id = aws_route_table.Son_rf.id
+  route_table_id = aws_route_table.Son_router_nt.id
 }
 
