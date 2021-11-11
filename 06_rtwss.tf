bae740d7df65d9cc02a2688fc4e92d94da1408b6
erty7640@gmail.com <erty7640@gmail.com>
2021-11-11 15:30:52 +0900

    step1

---
+++ 06_rtwss.tf
@@ -0,0 +1,10 @@
+resource "aws_route_table_association" "Son_rtass_a"{
+  subnet_id = aws_subnet.Son_puba.id
+  route_table_id = aws_route_table.Son_rf.id
+}
+
+resource "aws_route_table_association" "Son_rtass_c" {
+  subnet_id = aws_subnet.Son_pubc.id
+  route_table_id = aws_route_table.Son_rf.id
+}
+
