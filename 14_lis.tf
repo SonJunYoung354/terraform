3537ccdffb87092d8484c7dbd547e0c970820fb5
erty7640@gmail.com <erty7640@gmail.com>
2021-11-12 12:37:41 +0900

    test

---
+++ 14_lis.tf
@@ -0,0 +1,13 @@
+##
+##
+resource "aws_lb_listener" "Son_list" {
+    load_balancer_arn = aws_lb.Son_lb.arn
+    port = "80"
+    protocol = "HTTP"
+
+    default_action {
+      type = "forward"
+      target_group_arn = aws_lb_target_group.Son_target.arn
+    }
+  
+}
